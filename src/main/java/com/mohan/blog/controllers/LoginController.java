2fb893a2d98594649492f0c671cbf54465e84e06
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-06-15 10:53:10 +0530

    Add login page and controller

---
+++ src/main/java/com/mohan/blog/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.mohan.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+}
